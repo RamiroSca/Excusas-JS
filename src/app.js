c3a53fd6039514c0068797d9538227878780e921
RamiroSca <151871077+RamiroSca@users.noreply.github.com>
2023-12-09 14:41:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generatExcuse = () => {
+  let quien = ["El perro", "Mi abuela", "El cartero", "Mi pajaro"];
+  let accion = ["comio", "orino", "llevo", "defeco"];
+  let que = ["mi tarea", "mi celular", "mi auto"];
+  let cuando = [
+    "antes de la clase",
+    "cuando dormia",
+    "mientras hacia ejercicio",
+    "durante el almuerzo",
+    "mientras oraba"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let q = Math.floor(Math.random() * quien.length);
+  let a = Math.floor(Math.random() * accion.length);
+  let qu = Math.floor(Math.random() * que.length);
+  let c = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[q] + " se " + accion[a] + " en " + que[qu] + " justo " + cuando[c]
+  );
 };
+//write your code here
